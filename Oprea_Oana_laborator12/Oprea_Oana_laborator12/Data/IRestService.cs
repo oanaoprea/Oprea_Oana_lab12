f35e1bc8316dbfb4ce2a999c6de3f4a30d2b95b5
oana oprea <oanamaria.oprea@yahoo.com>
2022-01-15 16:50:38 +0200

    Add project files.

---
+++ Oprea_Oana_laborator12/Oprea_Oana_laborator12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Oprea_Oana_laborator12.Models;
+
+namespace Oprea_Oana_laborator12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
